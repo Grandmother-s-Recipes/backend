9dd9c86aa06299ccf390682143194578b4667615
DanielWilliamParsons <parsonsdwp@gmail.com>
2024-10-25 11:45:37 +0900

    fix add cors options

--- server/server.js
+++ server/server.js
@@ -3,14 +3,21 @@ const session = require('express-session');
 const MemoryStore = require('memorystore')(session);
 const bcrypt = require('bcrypt');
 const axios = require('axios');
-const knex = require('./knex'); 
+const knex = require('./knex');
 const cors = require('cors');
+
 const app = express();
 
 
+// const corsOptions = {
+//   origin: 'http://localhost:5173/',
+//   optionsSuccessStatus: 200,
+//   credentials: true
+// };
+
+app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use(cors());
 
 
 app.use(session({
