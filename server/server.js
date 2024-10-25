be660c2ddc69db649d4cfecfb17f35973c0c7314
DanielWilliamParsons <parsonsdwp@gmail.com>
2024-10-25 11:55:27 +0900

    test: testing cors

--- server/server.js
+++ server/server.js
@@ -7,11 +7,11 @@ const cors = require('cors');
 
 const app = express();
 
-// const corsOptions = {
-//   origin: 'http://localhost:5173',
-//   optionsSuccessStatus: 200,
-//   credentials: true
-// };
+const corsOptions = {
+  origin: 'http://localhost:5173/',
+  optionsSuccessStatus: 200,
+  credentials: true
+};
 
 app.use(cors());
 app.use(express.json());
