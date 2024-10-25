65e40f54fac41e3f8cee2027d087d3a314db8e28
DanielWilliamParsons <parsonsdwp@gmail.com>
2024-10-25 12:03:31 +0900

    fix: cors

--- server/server.js
+++ server/server.js
@@ -8,12 +8,12 @@ const cors = require('cors');
 const app = express();
 
 const corsOptions = {
-  origin: 'http://localhost:5173/',
+  origin: 'http://localhost:5173',
   optionsSuccessStatus: 200,
   credentials: true
 };
 
-app.use(cors());
+app.use(cors(corsOptions));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
